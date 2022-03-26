7469a970c55876d95d14bbc7e91dab71c954262c
DreVinciCode <dre.vivero@gmail.com>
2022-03-25 20:44:24 -0400

    hand gesture

--- Assets/Scripts/HandDetection.cs
+++ Assets/Scripts/HandDetection.cs
@@ -13,14 +13,19 @@ public class HandDetection : MonoBehaviour
 {
     public GameObject leftHandIndexMarker;
     public GameObject rightHandIndexMarker;
+    public GameObject Portal;
     public Transform MarkerParent;
+    public float scaleValue = 1f;
 
     public Vector3 offset;
 
     private bool _portalsummonCheck;
     private bool _markerCheck;
 
+    public float minscale = 0f;
+    public float maxscale = 50f;
 
+    private bool fullscale = false;
     private bool quad2 = false;
     private bool quad3 = false;
     private bool quad4 = false;
@@ -77,21 +82,29 @@ public class HandDetection : MonoBehaviour
 
                 _portalsummonCheck = true;
 
-                if (HandJointUtils.TryGetJointPose(TrackedHandJoint.Wrist, Handedness.Right, out wristPose) && _portalsummonCheck && _markerCheck)
+                if (HandJointUtils.TryGetJointPose(TrackedHandJoint.IndexTip, Handedness.Right, out wristPose) && _portalsummonCheck && _markerCheck)
                 {
                     _markerPosition = wristPose.Position + offset;
                     Instantiate(leftIndexObject, _markerPosition, wristPose.Rotation, MarkerParent);
- 
+                    //Instantiate(Portal, _markerPosition, wristPose.Rotation, MarkerParent);
                     _markerCheck = false;
                 }
 
 
+
+               /*
+                if(distance >= maxscale)
+                {
+
+                }
+               */
+
                 //check if hand follows gesture behavior
-                if(indexPose.Position.y > _markerPosition.y && indexPose.Position.x < _markerPosition.x)
+                if (indexPose.Position.y > _markerPosition.y && indexPose.Position.x < _markerPosition.x)
                 {
                     quad2 = true;
                     _portalValue = 1;
-                    Debug.Log("2");
+                    //Debug.Log("2");
                 }
 
                 if(quad2 && indexPose.Position.y < _markerPosition.y && indexPose.Position.x < _markerPosition.x)
@@ -99,7 +112,7 @@ public class HandDetection : MonoBehaviour
                     quad3 = true;
                     _portalValue = 2;
 
-                    Debug.Log("3");
+                    //Debug.Log("3");
 
                 }
                 if (quad3 && indexPose.Position.y < _markerPosition.y && indexPose.Position.x > _markerPosition.x)
@@ -107,7 +120,7 @@ public class HandDetection : MonoBehaviour
                     quad4 = true;
                     _portalValue = 3;
 
-                    Debug.Log("4");
+                    //Debug.Log("4");
 
                 }
                 if (quad4 && indexPose.Position.y > _markerPosition.y && indexPose.Position.x > _markerPosition.x)
@@ -115,19 +128,41 @@ public class HandDetection : MonoBehaviour
                     quad1 = true;
                     _portalValue = 4;
 
-                    Debug.Log("1");
+                    //Debug.Log("1");
+                }
+
+                if (quad1 && quad2 && quad3 && quad4)
+                {
+                    IncreasePortal();
 
                 }
+
+            }
+            else
+            {
+                DecreasePortal();
+                quad1 = false;
+                quad2 = false;
+                quad3 = false;
+                quad4 = false;
 
 
             }
 
             _portalsummonCheck = false;
+            DestroyChildren(MarkerParent);
+
         }
         else 
         {
             DestroyChildren(MarkerParent);
             _markerCheck = true;
+            //DecreasePortal();
+            quad1 = false;
+            quad2 = false;
+            quad3 = false;
+            quad4 = false;
+
         }
 
         //detect three fingers and summon empty below wrist
@@ -135,6 +170,21 @@ public class HandDetection : MonoBehaviour
 
     }
 
+    private void IncreasePortal()
+    {
+        if (Portal.transform.localScale.x < maxscale)
+            Portal.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+        if(Portal.transform.localScale.x >= maxscale)
+        {
+            fullscale = true;
+        }
+    }
+
+    private void DecreasePortal()
+    {
+        if (Portal.transform.localScale.x > minscale && !fullscale)
+            Portal.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+    }
 
     private void DestroyChildren(Transform parent)
     {
